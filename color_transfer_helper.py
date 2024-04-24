6fce17a0a1dadc6239bb32ef8382e653cbcca0cc
shanzhaiii <alexandre.riou@epfl.ch>
2024-04-25 01:29:18 +0200

    add moving color checker pattern to correct image location with affine transformation

--- color_transfer_helper.py
+++ color_transfer_helper.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from typing import Tuple
 
+import numpy as np
+
 
 @dataclass
 class ColorCheckerLocation:
@@ -13,3 +15,13 @@ class ColorCheckerLocation:
 
     def is_initialized(self):
         return self.top_left is not None and self.top_right is not None and self.bottom_left is not None
+
+    @staticmethod
+    def build_from_pattern(pattern: np.ndarray) -> 'ColorCheckerLocation':
+        return ColorCheckerLocation((0, 0), (0, pattern.shape[0]), (pattern.shape[1], 0))
+
+    def to_np_array(self) -> np.ndarray:
+        return np.float32((self.top_left, self.top_right, self.bottom_left))
+
+    def __repr__(self):
+        return f'TopLeft: {self.top_left}, TopRight: {self.top_right}, Bottom_Left: {self.bottom_left}'
