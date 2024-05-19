48ac640e457db4a6ae1fc86c2d4a0e236f472816
Alexandre Riou <alexandre.riou@epfl.ch>
2024-05-19 22:52:17 +0200

    add uniform number generation to cuda kernel

--- pycuda_test.py
+++ pycuda_test.py
@@ -1,11 +1,12 @@
 import pycuda.gpuarray as gpuarray
 import pycuda.driver as drv
 import pycuda.autoinit
+from pycuda.compiler import SourceModule
+from pycuda import curandom
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from pycuda import curandom
 
 if (os.system("cl.exe")):
     os.environ[
@@ -14,26 +15,16 @@ if (os.system("cl.exe")):
     raise RuntimeError("cl.exe still not found, path probably incorrect")
 
 
-# Sets the seed, for reproducibility
-def set_seeder(N, seed):
-    seedarr = (
-            gpuarray.ones_like(gpuarray.zeros(N, dtype=np.int32), dtype=np.int32) * seed
-    )
-    return seedarr
-
-
-from pycuda.compiler import SourceModule
-
 func_mod = SourceModule("""
 #include <curand_kernel.h>
 extern "C" {
-    __global__ void func(float pois_lambda, 
-    int *pois_rand, 
-    curandState *global_state)
+    __global__ void func(float *pois_lambda, float *uniform_out,
+    int *pois_rand, curandState *global_state)
     {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         curandState local_state = global_state[idx];
-        pois_rand[idx] = curand_poisson(&local_state, pois_lambda);
+        pois_rand[idx] = curand_poisson(&local_state, pois_lambda[idx]);
+        uniform_out[idx] = curand_uniform(&local_state);
         global_state[idx] = local_state;
     }
 }
@@ -41,35 +32,33 @@ extern "C" {
 
 func = func_mod.get_function('func')
 
-# Define constants
-seed = 6
-size = 1024
-pois_lambda = 10.7
-block_size = 64
+if __name__ == '__main__':
+    # Define constants
+    size = 4
+    block_size = 64
 
-# Allocate memory on gpu
-sample_gpu_holder = gpuarray.empty(size, dtype=np.int32)
-# Define the random number generator
-_generator = curandom.XORWOWRandomNumberGenerator(
-    lambda N: set_seeder(N, seed)
-)
-max_size = _generator.generators_per_block
-if size // max_size > 1:
-    raise ValueError("Too many generators expected")
-
-func(
-        np.float32(pois_lambda),
-        sample_gpu_holder,
-        _generator.state,
-        block=(block_size, 1, 1),
-        grid=(size // block_size + 1, 1),
+    # Allocate memory on gpu
+    lambdas = np.array([1,2,3,4], dtype=np.float32)
+    lambdas_gpu = gpuarray.to_gpu(lambdas)
+    sample_gpu_holder = gpuarray.empty(size, dtype=np.int32)
+    uniform_gpu_holder = gpuarray.empty(size, dtype=np.float32)
+    # Define the random number generator
+    _generator = curandom.XORWOWRandomNumberGenerator(
+        curandom.seed_getter_unique
     )
 
-# Retrieve memory from GPU
-sample_gpu_returned = sample_gpu_holder.get()
-sample_cpu = np.random.poisson(pois_lambda, size)
-hbins = np.arange(20)
-plt.hist(sample_gpu_returned, label="GPU samples", alpha=0.5, bins=hbins)
-plt.hist(sample_cpu, label="CPU samples", alpha=0.5, bins=hbins)
-plt.legend()
-plt.savefig("gpu_poisson.png")
+    func(
+            lambdas_gpu,
+            uniform_gpu_holder,
+            sample_gpu_holder,
+            _generator.state,
+            block=(block_size, 1, 1),
+            grid=(size // block_size + 1, 1),
+        )
+
+    # Retrieve memory from GPU
+    sample_gpu_returned = sample_gpu_holder.get()
+    uniform = uniform_gpu_holder.get()
+    print(sample_gpu_returned)
+    print(uniform)
+
