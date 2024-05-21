99466ad2e60247ded7dad69df8b69e9f7f752d7c
Alexandre Riou <alexandre.riou@epfl.ch>
2024-05-21 17:17:32 +0200

    fix cuda bad curandom, bad accessing, bad gaussian coords

--- pycuda_test.py
+++ pycuda_test.py
@@ -16,12 +16,16 @@ EPSILON_GREY_LEVEL = 0.1
 
 # arguments of the algorithm
 file_name_in = "data/digital/small.png"
-file_name_out = "data/test_small_modified_algo.png"
+file_name_out = "gpu.png"
 
 
 func_mod = SourceModule("""
 #include <curand_kernel.h>
 #include <math.h>
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
 extern "C" {
     __global__ void func(float *pois_lambda, int *pois_rand, int N,
     float *x_gaussian, float *y_gaussian, float ag, int n_monte_carlo
@@ -29,53 +33,60 @@ extern "C" {
     {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
         int idy = blockIdx.y * blockDim.y + threadIdx.y;
-        int pix = 0;
-        int pt_covered = 0;
-        
-        float sigmaFilter = 0.8;
-        float maxRadius = 0.025;
-        
-        for (int i=0; i<n_monte_carlo;++i){
-            float xGaussian = idx + sigmaFilter*(x_gaussian[i])/ag;
-            float yGaussian = idy + sigmaFilter*(y_gaussian[i])/ag;
+        if ((idx < width) && (idy < height))
+        {
+            int pix = 0;
+            int pt_covered = 0;
+
             
-            //determine the bounding boxes around the current shifted pixel
-            int minX = floor( (xGaussian - maxRadius)/ag);
-            int maxX = floor( (xGaussian + maxRadius)/ag);
-            int minY = floor( (yGaussian - maxRadius)/ag);
-            int maxY = floor( (yGaussian + maxRadius)/ag);
-            
-            for(int ncx = minX; ncx <= maxX; ncx++)
-            {
-                if (pt_covered)
-                    break;
-                for(int ncy = minY; ncy <= maxY; ncy++)
+            for (int i=0; i<n_monte_carlo;++i){
+                float xGaussian = idx + 0.5 + sigma_filter * x_gaussian[i];
+                float yGaussian = idy + 0.5 + sigma_filter * y_gaussian[i];
+                
+                //determine the bounding boxes around the current shifted pixel
+                int minX = floor((xGaussian - grain_radius) / ag);
+                int maxX = floor((xGaussian + grain_radius) / ag);
+                int minY = floor((yGaussian - grain_radius) / ag);
+                int maxY = floor((yGaussian + grain_radius) / ag);
+                
+                for(int ncx = minX; ncx <= maxX; ncx++)
                 {
                     if (pt_covered)
                         break;
-                    unsigned long seed = 120;
-                    curandState local_state;
-                    curand_init(seed, ncx, ncx, &local_state);
-                    
-                    float u = pois_lambda[idy*N+idx];
-                    int ncell = curand_poisson(&local_state, u);
-                    for (int k = 0; k < ncell; k++)
+                    for(int ncy = minY; ncy <= maxY; ncy++)
                     {
-                        float xCentreGrain = ncx*ag + ag*curand_uniform(&local_state);
-                        float yCentreGrain = ncy*ag + ag*curand_uniform(&local_state);
-                        
-                        if ((xCentreGrain-xGaussian)*(xCentreGrain-xGaussian) + (yCentreGrain-yGaussian)*(yCentreGrain-yGaussian) < maxRadius * maxRadius)
-                        {
-                            ++pix;
-                            pt_covered = 1;
+                        if (pt_covered)
                             break;
+                        float cell_corner_x = ag * ncx;
+                        float cell_corner_y = ag * ncy;
+                        unsigned long seed = 120;
+                        curandState local_state;
+                        
+                        curand_init(seed, ncx, ncy, &local_state);
+                        
+                        
+                        int x_img = MAX(MIN(floor(cell_corner_x),width-1),0);
+                        int y_img = MAX(MIN(floor(cell_corner_y),height-1),0);
+                        float u = pois_lambda[y_img*width + x_img];
+                        int ncell = curand_poisson(&local_state, u);
+                        for (int k = 0; k < ncell; k++)
+                        {
+                            float xCentreGrain = cell_corner_x + ag * curand_uniform(&local_state);
+                            float yCentreGrain = cell_corner_y + ag * curand_uniform(&local_state);
+                            
+                            if ((xCentreGrain-xGaussian)*(xCentreGrain-xGaussian) + (yCentreGrain-yGaussian)*(yCentreGrain-yGaussian) < grain_radius * grain_radius)
+                            {
+                                ++pix;
+                                pt_covered = 1;
+                                break;
+                            }
                         }
                     }
                 }
+                pt_covered = 0;
             }
-            pt_covered = 0;
-        }
-        pois_rand[idy*N+idx]=pix;
+            pois_rand[idy*width+idx]=pix;
+        }    
     }
 }
 """, no_extern_c=True)
@@ -113,6 +124,10 @@ if __name__ == '__main__':
     x_gaussian_list = np.random.normal(0.0, sigma_filter, n_monte_carlo).astype(np.float32)
     y_gaussian_list = np.random.normal(0.0, sigma_filter, n_monte_carlo).astype(np.float32)
 
+    # cuda parameters
+    block_size = (16, 16, 1)
+    grid = (math.ceil(height_in / block_size[0]), math.ceil(width_in / block_size[1]))
+
     full_img = []
 
     for color_channel in range(3):
@@ -130,17 +145,20 @@ if __name__ == '__main__':
             lambdas_gpu,
             sample_gpu_holder,
             np.int32(width_in),
+            np.int32(height_in),
             x_gaussian_list_gpu,
             y_gaussian_list_gpu,
             np.float32(ag),
             np.int32(n_monte_carlo),
-            block=(16, 16, 1),
-            grid=(16, 16),
+            np.float32(sigma_filter),
+            np.float32(mu_r),
+            block=block_size,
+            grid=grid,
             )
 
         # Retrieve memory from GPU
         full_img.append(sample_gpu_holder.get())
 
-    final_img = ((np.dstack(full_img)/100) * (MAX_GREY_LEVEL + EPSILON_GREY_LEVEL)).astype(np.uint8)
+    final_img = ((np.dstack(full_img)/n_monte_carlo) * (MAX_GREY_LEVEL + EPSILON_GREY_LEVEL)).astype(np.uint8)
     image_out = Image.fromarray(final_img)
     image_out.save(file_name_out)
