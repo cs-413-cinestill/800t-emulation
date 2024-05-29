d0863e2a377033648afa4de42cb0082d92626668
shanzhaiii <56897608+shanzhaiii@users.noreply.github.com>
2024-05-29 12:50:14 +0200

    Color transfer (#1)
    * add documentation and move unused code
    
    * add color checker overlay function
    
    * add color checker position fetching
    
    * add moving color checker pattern to correct image location with affine transformation
    
    * swap affine for perspective transform in CC alignement
    
    * implement extract and plot patch functions
    
    * add white level integration
    
    * start tif processing pipeline
    
    * refactor colorchecker into seperate class
    
    * add ColorCheckerReadings skeleton
    
    * refactor patch calculation into ColorCheckerReading
    
    * fix float and int incompatible in patch extraction and plotting
    
    * implement color transfer matrix on supposedly linear data
    
    * change color transfer implementation to work on gamma corrected data
    
    * test color transfer function on new image
    
    * tif linearisation with srg gamma inverse function
    
    * add todos
    
    * implement non linear function curve analysis
    
    * add tif linearization
    
    * implement color transfer function with linearized tif
    
    * implement color transfer skeleton
    
    * continue color transfer class definition
    
    * full color transfer implementation
    
    * add linear component to exponential linearization and work on post-processed image
    
    * simplify color matrix calculation and add todos
    
    * implement rawpy wrapper
    
    * change TE226 color checker dimensions
    
    * fix bad whitebalance implementation in digitalimage.py
    
    * implement upscaled thumbnail to be usable with color chart extraction
    
    * add todos
    
    * refactor locating and plotting patches into ColorCheckerReading
    
    * fix accidentally declaring static variables in colortransfer.py and digitalimage.py
    
    * Add abstract class LinearizeFunction
    
    * add exponential definition
    
    * fix luminosity in linearization.py
    
    * implement luminosity and comparison plots in linearization
    
    * implement exponential with linear component linearization
    
    * modify ColorCheckerReading apply_transformation to take as input an apply_inv function
    
    * add save linearize function to file capability
    
    * refactor data folder as dataset and other cosmetic changes
    
    * add dataset to github repo with gitlfs
    
    * add dataset to github repo with gitlfs
    
    * implement color_transfer_calculator.ipynb stub and refactor ColorCheckerReading name
    
    * implement color checker reading saving and loading
    
    * change colorchecker numpy visualisation to cached property and implement very light color checker reading saving
    
    * Remove all git lfs files
    
    * add dataset as gdrive link and continue calculator
    
    * Implement TE226 in color_transfer_calculator.ipynb
    
    * replace color_transfer test file with real demo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 800t-emulation
+
+Download our dataset from
+https://drive.google.com/drive/folders/1o8s59JwpYBfTTskVF3c7DCLGVQAuGsFa?usp=drive_link
+
+Coded for Python 3.10+
---
+++ digitalimage.py
@@ -0,0 +1,36 @@
+import rawpy
+import numpy as np
+from rawpy._rawpy import FBDDNoiseReductionMode, ColorSpace
+from PIL import Image
+import io
+
+class DigitalImage:
+    """
+    A digital image container for 12 bit RW2 panasonic raw files
+
+    Attributes:
+        raw: the raw image as height x width x 3 numpy array (12 bit dataset as float)
+        thumbnail: the JPEG thumbnail included in the raw image as height x width x 3 numpy array (8 bit as float)
+    """
+
+    def __init__(self, path: str, exposure: float = 0):
+        """
+        load a Digital Image from path, with optional exposure compensation
+        :param path: path to .RW2 file
+        :param exposure: exposure compensation as a float in .25 increments
+        """
+        white = [1, 1, 1, 1]
+        with rawpy.imread(path) as raw:
+            self.raw: np.ndarray = raw.postprocess(
+                fbdd_noise_reduction=FBDDNoiseReductionMode.Light,
+                output_color=ColorSpace.raw,
+                output_bps=16,
+                no_auto_scale=False, no_auto_bright=True, exp_shift=exposure,
+                gamma=(1, 1),
+                user_wb=white,use_camera_wb=False, use_auto_wb=False) / (2**16)
+            width = self.raw.shape[1]
+            height = self.raw.shape[0]
+
+            self.thumbnail: np.ndarray = np.array(
+                Image.open(io.BytesIO(raw.extract_thumb().data)).resize((width, height))
+            ) / 255
